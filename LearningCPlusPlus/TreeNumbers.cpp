774a24d3b846d90ca4d46b32d6700d91d0def0b0
Dennis Hoerner <hoernerdennis@gmail.com>
2020-02-14 01:13:24 +0100

    inital commit

---
+++ LearningCPlusPlus/TreeNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "function_declaration.h"
+
+using namespace std;
+
+void TreeNumbers()
+{
+   int print_number;
+   print_number = 5;
+   cout<< "Hello TreeNumbers" << print_number;
+
+}
---
+++ LearningCPlusPlus/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "function_declaration.h"
+
+/* in Main Function add different functions which will be called for tasks */
+/* for disabling and enabling use commando arguments   */
+
+using namespace std;
+
+#define ENABLE 1
+#define DISABLE 0
+
+int main()
+{
+
+   /*Build Tree of Numbers */
+   #if ( ENABLE )
+   TreeNumbers();
+   #endif
+
+
+
+
+
+   return 0;
+}
